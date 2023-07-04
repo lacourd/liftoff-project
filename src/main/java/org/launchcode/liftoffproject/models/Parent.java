e1a468dfdc14ab8cb883de4e4077227be130fffb
AudraHartwell <a.hartwell013@gmail.com>
2023-07-04 18:28:55 -0400

    user-authentication-trial-1

---
+++ src/main/java/org/launchcode/liftoffproject/models/Parent.java
@@ -0,0 +1,9 @@
+package org.launchcode.liftoffproject.models;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class Parent extends AbstractEntity{
+
+
+}
---
+++ src/main/java/org/launchcode/liftoffproject/models/User.java
@@ -0,0 +1,32 @@
+package org.launchcode.liftoffproject.models;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+
+
+@Entity
+public class User extends AbstractEntity{
+
+    @NotNull
+    private String username;
+
+    @NotNull
+    private String pwHash;
+
+    private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    public User() {}
+
+    public User(String username, String password) {
+        this.username = username;
+        this.pwHash = encoder.encode(password);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public boolean isMatchingPassword(String password) {
+        return encoder.matches(password, pwHash);
+    }
+}
