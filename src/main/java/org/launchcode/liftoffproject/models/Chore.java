e269fb55276c62ed3b9cf179e191ba342e0a6d4a
Darren LaCour <46571147+lacourd@users.noreply.github.com>
2023-07-09 17:01:40 -0500

    Some temp changes to Chore.java that set the childAssigned field to a single child isntead of a List

--- src/main/java/org/launchcode/liftoffproject/models/Chore.java
+++ src/main/java/org/launchcode/liftoffproject/models/Chore.java
@@ -2,6 +2,7 @@ package org.launchcode.liftoffproject.models;
 
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotBlank;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,8 +16,9 @@ public class Chore extends AbstractEntity{
     @NotBlank
     private String choreDescription;
 
-//    @ManyToMany
-    private List<Child> childAssigned = new ArrayList<>();
+    @ManyToOne
+//    will need to be a List<Child> eventually if we want to be able to assign a chore to multiple children
+    private Child childAssigned;
 
     private Parent parentCreator;
 
@@ -31,7 +33,7 @@ public class Chore extends AbstractEntity{
     public Chore(String name, String choreDescription, int rewardPoints, String dueDay) {
         this.name = name;
         this.choreDescription = choreDescription;
-         this.rewardPoints = rewardPoints;
+        this.rewardPoints = rewardPoints;
         this.dueDay = dueDay;
         this.completed = false;
     }
@@ -52,11 +54,11 @@ public class Chore extends AbstractEntity{
         this.choreDescription = choreDescription;
     }
 
-    public List<Child> getChildAssigned() {
+    public Child getChildAssigned() {
         return childAssigned;
     }
 
-    public void setChildAssigned(List<Child> childAssigned) {
+    public void setChildAssigned(Child childAssigned) {
         this.childAssigned = childAssigned;
     }
 
