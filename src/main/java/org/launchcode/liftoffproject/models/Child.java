41ecafb0da0848e0cf762f9ca08a084b6b9b9fd4
mikezanger <michaelkzanger@gmail.com>
2023-07-10 18:40:31 -0500

    child and parent commit

--- src/main/java/org/launchcode/liftoffproject/models/Child.java
+++ src/main/java/org/launchcode/liftoffproject/models/Child.java
@@ -1,7 +1,46 @@
 package org.launchcode.liftoffproject.models;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class Child extends User {
+
+    @NotBlank(message = "Child's first name is required")
+    @Size(max = 50, message = "Child's name is too long")
+    private String firstName;
+
+    @NotBlank (message = "Child's last name is required")
+    @Size (max = 50, message = "Child's last name is too long")
+    private String lastName;
 
     private int points;
 
+
+    public Child(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
 }
--- src/main/java/org/launchcode/liftoffproject/models/Parent.java
+++ src/main/java/org/launchcode/liftoffproject/models/Parent.java
@@ -1,9 +1,53 @@
 package org.launchcode.liftoffproject.models;
 
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
 
 public class Parent extends User {
+
+    @NotBlank (message = "First Name is required")
+    @Size (max = 50, message = "Name is too long")
+    private String firstName;
+
+    @NotBlank (message = "Last Name is required")
+    @Size (max = 50, message = "Last Name is too long")
+    private String lastName;
+
+
+
+    @ManyToOne
+    public void Child() {
+        ArrayList<Object> children = new ArrayList<>();
+    }
+
+    public void Chores() {
+        ArrayList<Object> chores = new ArrayList<>();
+    }
+
+    public Parent(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 
 
 }
