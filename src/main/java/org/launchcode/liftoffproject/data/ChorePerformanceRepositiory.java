7cf926ab23d7d5a4d51284521ec29c2f7e31bcb1
Darren LaCour <46571147+lacourd@users.noreply.github.com>
2023-07-10 19:56:46 -0500

    Revert "Child Performance"
    This reverts commit cad746430b21714e30cf8011f104c762388004de.

--- src/main/java/org/launchcode/liftoffproject/data/ChorePerformanceRepositiory.java
+++
@@ -1,4 +0,0 @@
-package org.launchcode.liftoffproject.data;
-
-public interface ChorePerformanceRepositiory {
-}
--- src/main/java/org/launchcode/liftoffproject/data/kitambo.java
+++
@@ -1,6 +0,0 @@
-package org.launchcode.liftoffproject.data;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface kitambo extends CrudRepository<ChorePerformanceRepositiory, Integer> {
-}
