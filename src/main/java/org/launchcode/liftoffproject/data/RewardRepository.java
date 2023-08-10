1721436cf941590b46be9314d3b49b5dbafb5c59
Darren LaCour <46571147+lacourd@users.noreply.github.com>
2023-08-10 18:56:32 -0500

    Added Rewards to the Parent Dashboard

--- src/main/java/org/launchcode/liftoffproject/data/RewardRepository.java
+++ src/main/java/org/launchcode/liftoffproject/data/RewardRepository.java
@@ -1,9 +1,14 @@
 package org.launchcode.liftoffproject.data;
 
+import org.launchcode.liftoffproject.models.Parent;
 import org.launchcode.liftoffproject.models.Reward;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RewardRepository extends PagingAndSortingRepository<Reward, Integer> {
+
+//    List<Reward> findAllByParentCreatorAndRedeemed(Parent parent, boolean redeemed);
 }
