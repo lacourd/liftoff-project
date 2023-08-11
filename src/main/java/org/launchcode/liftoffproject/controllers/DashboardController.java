4599db518e2addc4be76ec0961487c87832bbf0d
Darren LaCour <46571147+lacourd@users.noreply.github.com>
2023-08-10 19:56:05 -0500

    Added the possibility of not assigning a child to a chore

--- src/main/java/org/launchcode/liftoffproject/controllers/DashboardController.java
+++ src/main/java/org/launchcode/liftoffproject/controllers/DashboardController.java
@@ -79,10 +79,10 @@ public class DashboardController {
             pageable = PageRequest.of(0, 5, Sort.by("id").ascending());
             Page<Chore> recentlyCompleted = choreRepository.findByParentCreatorAndCompleted(parent,true, pageable);
             model.addAttribute("recentlyCompleted", recentlyCompleted);
-            List<Reward> recentlyRedeemed = (List<Reward>) rewardRepository.findAllByParentCreatorAndRedeemed(parent, true);
-            model.addAttribute("recentlyRedeemed", recentlyRedeemed);
-            List<Reward> availableRewards = (List<Reward>) rewardRepository.findAllByParentCreatorAndRedeemed(parent, false);
-            model.addAttribute("availableRewards", availableRewards);
+//            List<Reward> recentlyRedeemed = (List<Reward>) rewardRepository.findAllByParentCreatorAndRedeemed(parent, true);
+//            model.addAttribute("recentlyRedeemed", recentlyRedeemed);
+//            List<Reward> availableRewards = (List<Reward>) rewardRepository.findAllByParentCreatorAndRedeemed(parent, false);
+//            model.addAttribute("availableRewards", availableRewards);
         }
         return "dashboard";
     }
